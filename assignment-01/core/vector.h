97f6f1b9a41edc2ff0017098e0f8d02747ea53d3
GeibTobias <geib02@gw.uni-passau.de>
2017-11-14 14:55:30 +0100

    vector

--- assignment-01/core/vector.h
+++ assignment-01/core/vector.h
@@ -7,7 +7,7 @@ namespace rt {
 
 class Float4;
 class Point;
-class Vector {
+class ALIGN(16) Vector {
     public:
         float x, y, z;
 
@@ -23,13 +23,13 @@ class Vector {
         Vector normalize() const{return Vector(this->x/this->length(),this->x/this->length(),this->x/this->length());}
 
 
-        Vector operator-() const;
+        Vector operator-() const{return Vector(this.x * -1.0f,this.y * -1.0f, this.z * -1.0f);}
 
-        float lensqr() const{ return Vector(this->x^2,this->y^2,this->z^2);}
-        float length() const; //find out how to sqrt
+        float lensqr() const{ return lsqr = this->x^2 + this->y^2 + this->z^2;}
+        float length() const{ return std::sqrt(lensqr());}
 
-        bool operator==(const Vector& b) const;
-        bool operator!=(const Vector& b) const;
+        bool operator==(const Vector& b) const{return (b.x == this->x && b.y == this->y && b.z == this->z);}
+        bool operator!=(const Vector& b) const{return !(b==this);}
 };
 
 Vector operator*(float scalar, const Vector& b);
@@ -38,7 +38,7 @@ Vector operator/(const Vector& a, float scalar);
 Vector cross(const Vector& a, const Vector& b);
 float dot(const Vector& a, const Vector& b);
 
-Vector min(const Vector& a, const Vector& b);
+Vector min(const Vector& a, const Vector& b){}
 Vector max(const Vector& a, const Vector& b);
 
 Point operator+(const Point& a, const Vector& b);
